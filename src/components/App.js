e22e208feb462d89d5e0a9626861e2c73bfa1658
tungatarovM <tungatarov.96@mail.ru>
2019-02-12 22:05:23 +0500

    delete App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Styles
-import './App.css';
+
 
 // Components
 import ProjectList from './ProjectList';
